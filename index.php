bad4ac1917354632347385d2633120a28e6a0ac5
Ioannis Kokkinis <info@upg.gr>
2016-06-08 17:53:09 +0300

    Updated index.phpmenu.php project.mobirise .

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="shortcut icon" href="assets/images/yourday-1703x917-11.png" type="image/x-icon">
   <meta name="description" content="">
-
+  
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:700,400&amp;subset=cyrillic,latin,greek,vietnamese">
   <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="assets/animate.css/animate.min.css">
@@ -17,9 +17,9 @@
   <link rel="stylesheet" href="assets/mobirise-gallery/style.css">
   <link rel="stylesheet" href="assets/mobirise-slider/style.css">
   <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
-
-
-
+  
+  
+  
 </head>
 <body>
 <section class="mbr-navbar mbr-navbar--freeze mbr-navbar--absolute mbr-navbar--sticky mbr-navbar--auto-collapse" id="menu-0">
@@ -28,15 +28,15 @@
             <div class="mbr-navbar__container">
                 <div class="mbr-navbar__column mbr-navbar__column--s mbr-navbar__brand">
                     <span class="mbr-navbar__brand-link mbr-brand mbr-brand--inline">
-
-                        <span class="mbr-brand__name"><a class="mbr-brand__name text-white" href="https://mobirise.com">Plan Your Day</a></span>
+                        
+                        <span class="mbr-brand__name"><a class="mbr-brand__name text-white" href="index.php">Plan Your Day</a></span>
                     </span>
                 </div>
                 <div class="mbr-navbar__hamburger mbr-hamburger"><span class="mbr-hamburger__line"></span></div>
                 <div class="mbr-navbar__column mbr-navbar__menu">
                     <nav class="mbr-navbar__menu-box mbr-navbar__menu-box--inline-right">
-                        <div class="mbr-navbar__column"><ul class="mbr-navbar__items mbr-navbar__items--right mbr-buttons mbr-buttons--freeze mbr-buttons--right btn-decorator mbr-buttons--active"><li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="index.php">Αρχικη</a></li><li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="index.php#msg-box3-6">Υπηρεσιες</a></li><li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="index.php#msg-box3-4">Σχετικα</a></li> <li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="menu.php">Μενου</a></li></ul></div>
-                        <div class="mbr-navbar__column"><ul class="mbr-navbar__items mbr-navbar__items--right mbr-buttons mbr-buttons--freeze mbr-buttons--right btn-inverse mbr-buttons--active"><li class="mbr-navbar__item"><a class="mbr-buttons__btn btn btn-default" href="index.php#form1-7">Επικοινωνια</a></li></ul></div>
+                        <div class="mbr-navbar__column"><ul class="mbr-navbar__items mbr-navbar__items--right mbr-buttons mbr-buttons--freeze mbr-buttons--right btn-decorator mbr-buttons--active"><li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="index.php">Αρχικη</a></li><li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="index.html#msg-box3-6">Υπηρεσιες</a></li><li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="index.html#msg-box3-4">Σχετικα</a></li> <li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="menu.php">Μενου</a></li> <li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="index.php#gallery1-38">Συνεργατες</a></li></ul></div>
+                        <div class="mbr-navbar__column"><ul class="mbr-navbar__items mbr-navbar__items--right mbr-buttons mbr-buttons--freeze mbr-buttons--right btn-inverse mbr-buttons--active"><li class="mbr-navbar__item"><a class="mbr-buttons__btn btn btn-default" href="index.html#form1-7">Επικοινωνια</a></li></ul></div>
                     </nav>
                 </div>
             </div>
@@ -44,17 +44,17 @@
     </div>
 </section>
 
-<section class="engine"><a rel="external" href="https://mobirise.com">free website creator software</a></section><section class="mbr-box mbr-section mbr-section--relative mbr-section--fixed-size mbr-section--full-height mbr-section--bg-adapted mbr-parallax-background" id="header1-1" style="background-image: url(assets/images/colour-1500x1000-44.jpg);">
-    <div class="mbr-box__magnet mbr-box__magnet--sm-padding mbr-box__magnet--center-center mbr-after-navbar">
+<section class="engine"><a rel="external" href="https://mobirise.com">mobile website generator</a></section><section class="mbr-box mbr-section mbr-section--relative mbr-section--fixed-size mbr-section--full-height mbr-section--bg-adapted mbr-parallax-background mbr-after-navbar" id="header1-1" style="background-image: url(assets/images/colour-1500x1000-44.jpg);">
+    <div class="mbr-box__magnet mbr-box__magnet--sm-padding mbr-box__magnet--center-center">
         <div class="mbr-overlay" style="opacity: 0.3; background-color: rgb(193, 193, 193);"></div>
         <div class="mbr-box__container mbr-section__container container">
             <div class="mbr-box mbr-box--stretched"><div class="mbr-box__magnet mbr-box__magnet--center-center">
                 <div class="row"><div class=" col-sm-8 col-sm-offset-2">
                     <div class="mbr-hero animated fadeInUp">
                         <h1 class="mbr-hero__text">PLAN YOUR DAY</h1>
-
+                        
                     </div>
-                    <div class="mbr-buttons btn-inverse mbr-buttons--center"><a class="mbr-buttons__btn btn btn-lg btn-danger animated fadeInUp delay" href="index.php#gallery2-8">Εκδηλωσεις</a> <a class="mbr-buttons__btn btn btn-lg animated fadeInUp delay btn-success" href="index.php#form1-7">Επικοινωνια</a></div>
+                    <div class="mbr-buttons btn-inverse mbr-buttons--center"><a class="mbr-buttons__btn btn btn-lg btn-danger animated fadeInUp delay" href="index.html#gallery2-8">Εκδηλωσεις</a> <a class="mbr-buttons__btn btn btn-lg animated fadeInUp delay btn-success" href="index.html#form1-7">Επικοινωνια</a></div>
                 </div></div>
             </div></div>
         </div>
@@ -71,7 +71,7 @@
         <div class="mbr-header mbr-header--wysiwyg row">
             <div class="col-sm-8 col-sm-offset-2">
                 <h3 class="mbr-header__text">ΑΝΑΛΑΜΒΑΝΟΥΜΕ ΤΗ ΔΕΞΙΩΣΗ ΣΑΣ</h3>
-
+                
             </div>
         </div>
     </div>
@@ -175,12 +175,12 @@
 </section>
 
 <section class="mbr-section mbr-section--relative mbr-section--fixed-size" id="msg-box3-4" style="background-color: rgb(255, 255, 255);">
-
+    
     <div class="mbr-section__container container mbr-section__container--first">
         <div class="mbr-header mbr-header--wysiwyg row">
             <div class="col-sm-8 col-sm-offset-2">
                 <h3 class="mbr-header__text">Plan Your Day</h3>
-
+                
             </div>
         </div>
     </div>
@@ -192,14 +192,14 @@
     <div class="mbr-section__container container mbr-section__container--last">
         <div class="row">
             <div class="col-sm-8 col-sm-offset-2">
-                <div class="mbr-buttons mbr-buttons--center"><a class="mbr-buttons__btn btn btn-lg btn-default" href="index.php#form1-7">Επικοινωνια</a></div>
+                <div class="mbr-buttons mbr-buttons--center"><a class="mbr-buttons__btn btn btn-lg btn-default" href="index.html#form1-7">Επικοινωνια</a></div>
             </div>
         </div>
     </div>
 </section>
 
 <section class="mbr-section mbr-section--relative mbr-section--fixed-size" id="social-buttons2-5" style="background-color: rgb(240, 240, 240);">
-
+    
 
     <div class="mbr-section__container container">
         <div class="mbr-header mbr-header--inline row">
@@ -212,53 +212,150 @@
 </section>
 
 <section class="mbr-section mbr-section--relative mbr-section--fixed-size" id="msg-box3-6" style="background-color: rgb(255, 255, 255);">
-
+    
     <div class="mbr-section__container container mbr-section__container--first">
         <div class="mbr-header mbr-header--wysiwyg row">
             <div class="col-sm-8 col-sm-offset-2">
                 <h3 class="mbr-header__text">Υπηρεσιες</h3>
-
+                
             </div>
         </div>
     </div>
     <div class="mbr-section__container container mbr-section__container--middle">
         <div class="row">
-            <div class="mbr-article mbr-article--wysiwyg col-sm-8 col-sm-offset-2"><ul><li>μεταφορά και στήσιμο εξοπλισμού με
-εξειδικευμένους εργάτες στο χώρο
-σας
+            <div class="mbr-article mbr-article--wysiwyg col-sm-8 col-sm-offset-2"><ul><li>μεταφορά και στήσιμο εξοπλισμού με 
+εξειδικευμένους εργάτες στο χώρο 
+σας 
 
-</li><li>ηχητική κάλυψη του χώρου </li><li>μουσική κάλυψη από dj
+</li><li>ηχητική κάλυψη του χώρου </li><li>μουσική κάλυψη από dj 
 
-</li><li>μουσικά σχήματα όπως καντάδες
+</li><li>μουσικά σχήματα όπως καντάδες 
 
-•εξειδικευμένο προσωπικό
-εξυπηρέτησης
+•εξειδικευμένο προσωπικό 
+εξυπηρέτησης 
 
-</li><li>φαγητό εξαιρετικής ποιότητας από
-chef επαγγελματίες με γνώσεις πάνω στο μπουφέ και το στήσιμο του
+</li><li>φαγητό εξαιρετικής ποιότητας από 
+chef επαγγελματίες με γνώσεις πάνω στο μπουφέ και το στήσιμο του 
 
-</li><li>κάβα ποτών, περιλαμβάνει πλήρη λίστα ποτών, εμφιαλωμένα τοπικά και μη
-κρασιά , αναψυκτικά
+</li><li>κάβα ποτών, περιλαμβάνει πλήρη λίστα ποτών, εμφιαλωμένα τοπικά και μη 
+κρασιά , αναψυκτικά 
 
-</li><li>welcome bar με menu από cocktails
+</li><li>welcome bar με menu από cocktails 
 
-</li><li>cheese &amp; wines menu
+</li><li>cheese &amp; wines menu 
 
-</li><li>animatrisse για την απασχόληση των παιδιών για όλη την διάρκεια της
+</li><li>animatrisse για την απασχόληση των παιδιών για όλη την διάρκεια της 
 δεξίωσης , του πάρτυ &nbsp;</li><li>μεταφορά με limo antique&nbsp;</li></ul></div>
         </div>
     </div>
     <div class="mbr-section__container container mbr-section__container--last">
         <div class="row">
             <div class="col-sm-8 col-sm-offset-2">
-                <div class="mbr-buttons mbr-buttons--center"><a class="mbr-buttons__btn btn btn-lg btn-default" href="index.php#form1-7">Επικοινωνια</a></div>
+                <div class="mbr-buttons mbr-buttons--center"><a class="mbr-buttons__btn btn btn-lg btn-default" href="index.html#form1-7">Επικοινωνια</a></div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="mbr-gallery mbr-section mbr-section--no-padding" id="gallery1-38" style="background-color: rgb(239, 239, 239);">
+    <!-- Gallery -->
+    <div class=" mbr-gallery-layout-default">
+        <div>
+            <div class="row mbr-gallery-row no-gutter">
+                <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 mbr-gallery-item">
+                    <a href="#lb-gallery1-38" data-slide-to="0" data-toggle="modal">
+                        <img alt="" src="assets/images/slide1-small.jpg">
+                        <span class="icon glyphicon glyphicon-zoom-in"></span>
+                    </a>
+                </div><div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 mbr-gallery-item">
+                    <a href="#lb-gallery1-38" data-slide-to="1" data-toggle="modal">
+                        <img alt="" src="assets/images/slide2-small.jpg">
+                        <span class="icon glyphicon glyphicon-zoom-in"></span>
+                    </a>
+                </div><div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 mbr-gallery-item">
+                    <a href="#lb-gallery1-38" data-slide-to="2" data-toggle="modal">
+                        <img alt="" src="assets/images/slide3-small.jpg">
+                        <span class="icon glyphicon glyphicon-zoom-in"></span>
+                    </a>
+                </div><div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 mbr-gallery-item">
+                    <a href="#lb-gallery1-38" data-slide-to="3" data-toggle="modal">
+                        <img alt="" src="assets/images/slide4-small.jpg">
+                        <span class="icon glyphicon glyphicon-zoom-in"></span>
+                    </a>
+                </div><div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 mbr-gallery-item">
+                    <a href="#lb-gallery1-38" data-slide-to="4" data-toggle="modal">
+                        <img alt="" src="assets/images/slide5-small.jpg">
+                        <span class="icon glyphicon glyphicon-zoom-in"></span>
+                    </a>
+                </div><div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 mbr-gallery-item">
+                    <a href="#lb-gallery1-38" data-slide-to="5" data-toggle="modal">
+                        <img alt="" src="assets/images/slide6-small.jpg">
+                        <span class="icon glyphicon glyphicon-zoom-in"></span>
+                    </a>
+                </div><div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 mbr-gallery-item">
+                    <a href="#lb-gallery1-38" data-slide-to="6" data-toggle="modal">
+                        <img alt="" src="assets/images/slide7-small.jpg">
+                        <span class="icon glyphicon glyphicon-zoom-in"></span>
+                    </a>
+                </div><div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 mbr-gallery-item">
+                    <a href="#lb-gallery1-38" data-slide-to="7" data-toggle="modal">
+                        <img alt="" src="assets/images/slide8-small.jpg">
+                        <span class="icon glyphicon glyphicon-zoom-in"></span>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="clearfix"></div>
+    </div>
+
+    <!-- Lightbox -->
+    <div data-app-prevent-settings="" class="mbr-slider modal fade carousel slide" tabindex="-1" data-keyboard="true" data-interval="false" id="lb-gallery1-38">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <ol class="carousel-indicators">
+                        <li data-app-prevent-settings="" data-target="#lb-gallery1-38" class=" active" data-slide-to="0"></li><li data-app-prevent-settings="" data-target="#lb-gallery1-38" data-slide-to="1"></li><li data-app-prevent-settings="" data-target="#lb-gallery1-38" data-slide-to="2"></li><li data-app-prevent-settings="" data-target="#lb-gallery1-38" data-slide-to="3"></li><li data-app-prevent-settings="" data-target="#lb-gallery1-38" data-slide-to="4"></li><li data-app-prevent-settings="" data-target="#lb-gallery1-38" data-slide-to="5"></li><li data-app-prevent-settings="" data-target="#lb-gallery1-38" data-slide-to="6"></li><li data-app-prevent-settings="" data-target="#lb-gallery1-38" data-slide-to="7"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="item active">
+                            <img alt="" src="assets/images/slide1.jpg">
+                        </div><div class="item">
+                            <img alt="" src="assets/images/slide2.jpg">
+                        </div><div class="item">
+                            <img alt="" src="assets/images/slide3.jpg">
+                        </div><div class="item">
+                            <img alt="" src="assets/images/slide4.jpg">
+                        </div><div class="item">
+                            <img alt="" src="assets/images/slide5.jpg">
+                        </div><div class="item">
+                            <img alt="" src="assets/images/slide6.jpg">
+                        </div><div class="item">
+                            <img alt="" src="assets/images/slide7.jpg">
+                        </div><div class="item">
+                            <img alt="" src="assets/images/slide8.jpg">
+                        </div>
+                    </div>
+                    <a class="left carousel-control" role="button" data-slide="prev" href="#lb-gallery1-38">
+                        <span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" role="button" data-slide="next" href="#lb-gallery1-38">
+                        <span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+
+                    <a class="close" href="#" role="button" data-dismiss="modal">
+                        <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+                        <span class="sr-only">Close</span>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
 </section>
 
 <section class="mbr-section mbr-section--relative mbr-section--fixed-size" id="form1-7" style="background-color: rgb(239, 239, 239);">
-
+    
     <div class="mbr-section__container mbr-section__container--std-padding container">
         <div class="row">
             <div class="col-sm-12">
@@ -271,7 +368,7 @@ chef επαγγελματίες με γνώσεις πάνω στο μπο
                             <div class="hide" data-form-alert-success="true">Ευχαριστουμε</div>
                         </div>
                         <form action="https://mobirise.com/" method="post" data-form-title="Επικοινωνια">
-                            <input type="hidden" value="bkjKgbglaN1LI02+r2Rl3hvMi1cKqrmge8B2bdTKQ/7p/hAFUJdDmrLoNkWlqbfN213SPsubtoNZ52ZMxg3tHNs/yLwKAjegk7ZCXWSBMYftp3Sggi0ndpGvVO3+k6wQ" data-form-email="true">
+                            <input type="hidden" value="4YQjsIKSB0l0AbkpGxtA1H7rMV3Clo9W7dyMHqZfS7+ZRkWNhuMDWwqkOylxWdQI1hLtZOx+kUSgTucnMO8Or9Lmn/OO7pvTfRQsoajDAgsBq7u+29YyDVZq1wui6zRp" data-form-email="true">
                             <div class="form-group">
                                 <input type="text" class="form-control" name="name" required="" placeholder="Ονομα*" data-form-field="Name">
                             </div>
@@ -307,7 +404,7 @@ chef επαγγελματίες με γνώσεις πάνω στο μπο
   <script src="assets/mobirise/js/script.js"></script>
   <script src="assets/mobirise-gallery/script.js"></script>
   <script src="assets/formoid/formoid.min.js"></script>
-
-
+  
+  
 </body>
 </html>
